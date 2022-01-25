3dd922b9943b9960261685b7a007d8b48e4e5bca
vedantkokate07 <vedant.kokate@somaiya.edu>
2022-01-25 19:28:28 +0530

    devops

---
+++ temp.py
@@ -0,0 +1,10 @@
+from random import shuffle
+
+adj=['nice','asshole','not nice','evil']
+sub=['manelkar','joydeep','arghyadeep','keshob']
+
+shuffle(adj)
+
+for i in range(len(sub)):
+    print(sub,'is',adj)
+    
