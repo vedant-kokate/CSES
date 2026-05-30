3a75b8a7c207595d7ef4d7e8354f9937ac4699f2
vedant-kokate <vedant.kokate@somaiya.edu>
2026-05-30 21:22:56 +0530

    updating the logic 1/n

--- Introductory Problems/Missing Number Vedant Kokate.py
+++ Introductory Problems/Missing Number Vedant Kokate.py
@@ -3,11 +3,14 @@ def get_ints(): return map(int, sys.stdin.readline().strip().split())
 def inputs():sys.stdin.readline()
 n=int(input())
 a=list(get_ints())
-ch=[0]*n
-for i in a:
-    ch[i-1]=1
-#print(ch)
-for i in range(n):
-    if ch[i]==0:
-        print(i+1)
-        break
+# # old logic
+# ch=[0]*n
+# for i in a:
+#     ch[i-1]=1
+# #print(ch)
+# for i in range(n):
+#     if ch[i]==0:
+#         print(i+1)
+#         break
+
+print((n*(n+1))//2 - sum(a))
