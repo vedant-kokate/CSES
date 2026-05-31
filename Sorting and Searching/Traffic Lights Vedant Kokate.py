9716c70dcca4c0928fa87ab3d9666286dff10712
vedant-kokate <vedant.kokate@somaiya.edu>
2026-05-31 21:33:40 +0530

    traffic light

--- Sorting and Searching/Traffic Lights Vedant Kokate.py
+++ Sorting and Searching/Traffic Lights Vedant Kokate.py
@@ -1,22 +1,39 @@
-r=range
-f=lambda:list(map(int,input().split()))
-x=f()
-p=f()
-l=[0]+sorted(p)+x
-print(l,'l')
-h=x[1]+2
-print('h',h)
-d={l[i]:[l[i-1],l[i+1]] for i in r(h)}
-print(d)
-m=max(l[i+1]-l[i] for i in r(h))
-print('m',m)
-k=[]
-for i in p[::-1]:
-    print(d)
-    k+=m,
-    a,b=d.pop(i)
-    print(a,b,'a b')
-    m=max(b-a,m)
-    d[a][1]=b
-    d[b][0]=a
-print(*k[::-1])
+import sys
+def get_ints(): return map(int, sys.stdin.readline().strip().split())
+ 
+
+def main():
+    x, n = get_ints()
+    p = list(get_ints())
+    ligths = {}
+    sorted_p = sorted(p)
+    max_distance = 0
+    for i in range(n):
+        # left, right
+        val = sorted_p[i]
+        left = sorted_p[i-1] if i-1 >= 0 else 0
+        right = sorted_p[i+1] if i+1 < n else x
+        ligths[val] = [left, right ]
+        max_distance = max(max_distance, (right - val), (val - left))
+    ligths[0] = [0 , sorted_p[0]]
+    ligths[x] = [sorted_p[n-1], x]
+    max_distance = max(max_distance, sorted_p[0] - 0, x - sorted_p[n-1])
+    # print(ligths)
+    # print("===========")
+    result = []
+    for i in reversed(p):
+        result.append(max_distance)
+        left, right = ligths[i]
+        max_distance = max(max_distance, (right -left))
+        # print(ligths[i])
+        # print(ligths[left], ligths[right])
+        ligths[right][0] = left
+        ligths[left][1] = right
+        # print(ligths)
+        # print("===========")
+
+    print(*reversed(result))
+
+
+if __name__ == "__main__":
+    main()
